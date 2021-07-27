3751542753cf1e681d776c7eaaab4494f47dc566
Kranthi <skranthi4444@gmail.com>
2021-07-27 15:04:41 +0530

    add all courses data

--- seeds/index.js
+++ seeds/index.js
@@ -1,17 +1,19 @@
 const Elective = require("../models/elective");
-const dummyData = require("./dummy_data.json");
-const ug_electives = require("./ug_electives_data.json");
-const pg_electives = require("./pg_electives_data.json");
+const dummyData = require("./dummy_data");
+const electivesData = require("./all_courses_data");
 const mongoose = require("mongoose");
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
-mongoose.connect(process.env.MONGO_DB_URL || "mongodb://localhost:27017/electives-review", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  process.env.MONGO_DB_URL || "mongodb://localhost:27017/electives-review",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -20,19 +22,12 @@ db.once("open", function () {
 });
 
 const seedDB = async () => {
-    await Elective.deleteMany({});
-    for(let elective of ug_electives) {
-        elective.reviews = [];
-        const el = new Elective(elective);
-        el.credits = el.credits % 5 + 1;
-        await el.save();
-    }
-    for(let elective of pg_electives) {
-        elective.reviews = [];
-        const el = new Elective(elective);
-        el.credits = el.credits % 5 + 1;
-        await el.save();
-    }
+  await Elective.deleteMany({});
+  for (let elective of electivesData) {
+    elective.reviews = [];
+    const el = new Elective(elective);
+    await el.save();
+  }
 };
 
 seedDB().then(() => {
